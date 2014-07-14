34e50bfafc18a6a2a16558f8988cd2cd4879c720
WalterPeck <chrislabarge2@gmail.com>
2014-07-13 20:52:51 -0400

    education thang

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,10 +7,11 @@ class StaticPagesController < ApplicationController
   
   
   def help
+  end
 
   
   
-  end
+  
 	
 	def score
 	end
--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,8 @@
 module StaticPagesHelper
+
+	def alphabet
+		letter= ('a'..'z').to_a.sample.upcase
+		letter
+	end
+
 end
