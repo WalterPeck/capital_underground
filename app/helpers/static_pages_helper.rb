5e148558ebd635e920be6aebfa44661f7691c02b
WalterPeck <chrislabarge2@gmail.com>
2014-07-24 17:14:40 -0400

    saving

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,8 +1,19 @@
 module StaticPagesHelper
 
-	def alphabet
+	def alphabet()
 		letter= ('a'..'z').to_a.sample.upcase
+		
 		letter
 	end
 
+	def new_letter(start_letter)
+	 	letter= alphabet
+	 	
+	 	while letter == start_letter do
+	 		letter= alphabet
+	 	end
+	 	
+	 	return letter
+	end
+	 	
 end
